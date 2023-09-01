e5632749b2bd61d5627ce2e099b7fe57791201aa
vivekkasturi <kasturi.vivek994@gmail.com>
2023-09-02 00:17:02 +0530

    Only Chessbox

--- src/App.js
+++ src/App.js
@@ -1,111 +1,36 @@
-import './App.css';
+export default function App() {
+  const m = 4;
+  const n = 5;
 
-import { useState } from 'react';
+  const arr1 = new Array(m).fill("");
+  const arr2 = new Array(n).fill("");
 
-import usePasswordGenerator from './Components/usePasswordGenerator';
-import StrengthStatus from './Components/StrengthStatus';
-
-
-
-
-//container
-//header
-//copy button
-//charaterlength and number
-//length-bar
-//4 checkboxs with text's
-//generate pasword button
-function App() {
-
-  const [length, setLength] = useState(4);
-  const [checkBoxData, setCheckBoxData] = useState([
-    { Name: "IncludeUpperCase", state: true },
-    { Name: "IncludeLowerCase", state: true },
-    { Name: "IncludeNumbers", state: true },
-    { Name: "IncludeSymbols", state: true }]);
-
-  const [cpyBtn, setCpyBtn] = useState(true);
-
-  const [strengthValue, setStrengthValue] = useState('');
-
-
-  const changecheckbox = (element) => {
-    console.log(element);
-    const updatedCheckBoxData = checkBoxData.map((e) => {
-      if (e.Name === element.Name) {
-
-        return { ...e, state: !element.state }
-
-
-      }
-
-      return e
-    });
-
-    setCheckBoxData(updatedCheckBoxData);
-  }
-  const { password, generatePassword } = usePasswordGenerator();
-
-
-  const copydata = () => {
-
-    if (password) {
-      navigator.clipboard.writeText(password);
-      setTimeout(() => {
-        alert("password is copied to clipboard,if you want new password please click generate password again ");
-        setCpyBtn(false);
-        setTimeout(()=>setCpyBtn(true),3000);
-      }, 1000);
-    }else{
-      alert("Please generate password to copy text!");
-    }
-
-  }
-
-  const changedatalength = (e) => {
-    setLength(e.target.value);
-
-  }
   return (
+    <div className="App">
 
-    <div className="Conatiner">
-      <div className="Header">
-        <h2>{password}</h2>
-        <button onClick={copydata}>{cpyBtn ? "copy" : "copied"}</button>
-      </div>
-
-      <div className="CharLength">
-        <h3>Character Length</h3>
-        <span>{length}</span>
-      </div>
-      <div className="CharLengthBar">
-
-        <input type="range" min="4" max="20" value={length} onChange={changedatalength} />
-
-      </div>
-      <div className="Checkboxwithtext">
-
-        {checkBoxData.map((element, index) => {
-
-          return (
-
-            <>
-              <li key={index}> <input type="checkbox" name={element} onChange={() => changecheckbox(element)} />{element.Name}</li>
-            </>
-          )
-        })}
-      </div>
-      <div className='StrengthStatus'>
-        <h3>Strength</h3>
-        <h3 className="StrengthStatus">{strengthValue}   <StrengthStatus length={length} strengthValue={strengthValue} setStrengthValue={setStrengthValue} /></h3>
-      </div>
-      <div className="GeneratePasswordBtn">
-        <button onClick={() => generatePassword(length, checkBoxData)}>Generate Password</button>
-      </div>
-
+      <table>
+        <tbody>
+          {arr1.map((ele, i) => {
+            return (
+              <tr className="outsidebox">
+                {arr2.map((ele, j) => {
+                  return (
+                    <td
+                      className="insidebox"
+                      style={{
+                        background: (i + j) % 2 === 0 ? "white" : "black"
+                      }}
+                    >
+                      {i}
+                      {j}
+                    </td>
+                  );
+                })}{" "}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-
   );
 }
-
-export default App;
